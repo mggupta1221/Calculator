da69965e0a01fd06b5626acc95cdc9634564c996
Mukesh Gupta <Mukesh.Gupta@Klingelnberg.com>
2022-10-06 16:36:39 +0530

    Add project files.

---
+++ Calculator/Cal.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cal
+{
+    class Calc
+    {
+     
+
+        public int addition(int number1, int number2)
+        {
+            return number1 + number2;
+        }
+        public int subtraction(int number1, int number2)
+        {
+            return number1 - number2;
+        }
+        public int multiplicatipon(int number1, int number2)
+        {
+            return number1* number2;
+
+        }
+        public double division(int number1, int number2)
+        {
+            return number1/ number2;
+        }
+    }
+}
---
+++ Calculator/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Cal;
+var c = new Calc();
+Console.WriteLine("Add:" + c.addition(5, 4));
+Console.WriteLine("sub:" + c.subtraction(5, 4));
+Console.WriteLine("mul:" + c.multiplicatipon(5, 4));
+Console.WriteLine("div:" + c.division(5, 4));
+
+
+
+
+
+
+
